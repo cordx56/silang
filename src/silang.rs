2a4f091138bbcbbf0eb4f37bbbee340d46b0520d
Kaoru Chisen <cordx56@cordx.net>
2021-02-22 08:49:29 +0900

    remove IdentifierValue

--- src/silang.rs
+++ src/silang.rs
@@ -5,63 +5,28 @@ pub mod define;
 
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum IdentifierType {
+#[derive(Clone)]
+pub struct UserDefinedFunction {
+    scope: usize,
+    statement: Statement,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum FactorKind {
     None,
+    Identifier,
     String,
     Int,
     Float,
     Bool,
     Function,
     TypeName,
-}
-
-#[derive(Debug, Clone)]
-pub struct UserDefinedFunction {
-    scope: usize,
-    statement: Statement,
-}
-
-#[derive(Clone)]
-pub struct IdentifierValue {
-    pub identifier_type: IdentifierType,
-    pub string: Option<String>,
-    pub int: Option<i64>,
-    pub float: Option<f64>,
-    pub bool: Option<bool>,
-    pub user_defined_function: Option<UserDefinedFunction>,
-    pub function: Option<fn (&mut Context, Vec<Factor>) -> Result<Vec<Factor>, String>>,
-    pub vector: Option<Vec<IdentifierValue>>,
-    pub map: Option<HashMap<String, IdentifierValue>>,
-}
-
-impl IdentifierValue {
-    pub fn new() -> IdentifierValue {
-        IdentifierValue {
-            identifier_type: IdentifierType::None,
-            string: None,
-            int: None,
-            float: None,
-            bool: None,
-            user_defined_function: None,
-            function: None,
-            vector: None,
-            map: None,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum FactorKind {
-    Identifier,
-    String,
-    Int,
-    Float,
-    Bool,
+    Vector,
+    Map,
     Expression,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Clone)]
 pub struct Factor {
     pub kind: FactorKind,
     pub name: Option<String>,
@@ -69,22 +34,44 @@ pub struct Factor {
     pub int: Option<i64>,
     pub float: Option<f64>,
     pub bool: Option<bool>,
+    pub vector: Option<Vec<Factor>>,
+    pub map: Option<HashMap<String, Factor>>,
     pub expression: Option<Expression>,
+    pub user_defined_function: Option<UserDefinedFunction>,
+    pub function: Option<fn (&mut Context, Vec<Factor>) -> Result<Vec<Factor>, String>>,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+impl Factor {
+    pub fn new() -> Factor {
+        Factor {
+            kind: FactorKind::None,
+            name: None,
+            string: None,
+            int: None,
+            float: None,
+            bool: None,
+            vector: None,
+            map: None,
+            expression: None,
+            user_defined_function: None,
+            function: None,
+        }
+    }
+}
+
+#[derive(Clone)]
 pub struct Expression {
     pub factors: Vec<Factor>,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Clone)]
 pub struct Statement {
     pub expression: Expression,
     pub statements: Vec<Statement>,
 }
 
 
-pub type IdentifierStorage = Vec<HashMap<String, IdentifierValue>>;
+pub type IdentifierStorage = Vec<HashMap<String, Factor>>;
 pub struct Context {
     pub scope: usize,
     pub identifier_storage: IdentifierStorage,
